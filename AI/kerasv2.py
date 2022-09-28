eb0d1ba30d5dd611555044b34cd1fc406ed2c2a0
kahre96 <kahre96@gmail.com>
2022-09-28 13:27:53 +0200

    Turned kerasv2 into a callable function and fixed variable transformation errors.

--- AI/kerasv2.py
+++ AI/kerasv2.py
@@ -51,22 +51,11 @@ class dataset():
 
         return train_data, val_data,test_data
 
-## directory where files is located
-dir = '/notebooks/test/images/'
-
-temp_dataset = dataset(dir,train_size = 0.7, test_size = 0.1, val_size = 0.2)
-created_dataset = temp_dataset.train_test_val()
-
-train,val,test = created_dataset
-
-x_train, y_train = zip(*train)
-x_val, y_val = zip(*val)
-x_test, y_test = zip(*test)
 
 transformer = alb.Compose([
     alb.RandomBrightnessContrast(brightness_limit=(-0.4,0.4), contrast_limit=(-0.4,0.4), p=0.4), ## brightness and contrast in range (-0.3 and 0.7)
     alb.HorizontalFlip(p=0.4), ## flip picture vertical
-    alb.geometric.rotate.Rotate(limit = [-5,5], p = 0.4), ## Rotate 10 degrees left or right 
+    alb.geometric.rotate.Rotate(limit = [-5,5], p = 0.4), ## Rotate 10 degrees left or right
     alb.RandomGamma(p=0.2)
 ])
 
@@ -85,62 +74,88 @@ def img_aug(image,label):
     return augmented_images,augmented_labels
 
 
-aug_train = img_aug(x_train,y_train)
-aug_val = img_aug(x_val,y_val)
-aug_test = img_aug(x_test,y_test)
+def kv2(bs):
+
+    ## directory where files is located
+    dir = 'cropped_dataset'
+
+    temp_dataset = dataset(dir,train_size = 0.7, test_size = 0.1, val_size = 0.2)
+    created_dataset = temp_dataset.train_test_val()
+
+    train,val,test = created_dataset
+
+    x_train, y_train = zip(*train)
+    x_val, y_val = zip(*val)
+    x_test, y_test = zip(*test)
+
+    x_train = list(x_train)
+    x_val = list(x_val)
+    x_test = list(x_test)
+    y_train = list(y_train)
+    y_val = list(y_val)
+    y_test = list(y_test)
 
 
-## extend training, val and test dataset with the augmentated images
-## train
-x_train.extend(aug_train[0])
-y_train.extend(aug_train[1])
-## val
-x_val.extend(aug_val[0])
-y_val.extend(aug_val[1])
-## test
-x_test.extend(aug_test[0])
-y_test.extend(aug_test[0])
+
+    aug_train = img_aug(x_train,y_train)
+    print(aug_train)
+    aug_val = img_aug(x_val,y_val)
+    print(aug_val)
+    aug_test = img_aug(x_test,y_test)
+    print(aug_test)
+    ## extend training, val and test dataset with the augmentated images
+    ## train
+    x_train.extend(aug_train[0])
+    y_train.extend(aug_train[1])
+    ## val
+    x_val.extend(aug_val[0])
+    y_val.extend(aug_val[1])
+    ## test
+    x_test.extend(aug_test[0])
+    y_test.extend(aug_test[1])
 
 
-## create the tensor dataset #keras V2 ;)
-## training
-x_train = tf.convert_to_tensor(x_train)
-y_train = tf.convert_to_tensor(y_train)
+    ## create the tensor dataset #keras V2 ;)
+    ## training
+    x_train = tf.convert_to_tensor(x_train)
+    y_train = tf.convert_to_tensor(y_train)
 
-##val
-x_val = tf.convert_to_tensor(x_val)
-y_val = tf.convert_to_tensor(y_val)
+    ##val
+    x_val = tf.convert_to_tensor(x_val)
+    y_val = tf.convert_to_tensor(y_val)
 
-##test
-x_test = tf.convert_to_tensor(x_test)
-y_test = tf.convert_to_tensor(y_test)
+    ##test
+    x_test = tf.convert_to_tensor(x_test)
+    y_test = tf.convert_to_tensor(y_test)
 
-## Make tensor slices. This step is done to create batches.
+    ## Make tensor slices. This step is done to create batches.
 
-#train
-x_train_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(x_train)
-y_train_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(y_train)
+    #train
+    x_train_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(x_train)
+    y_train_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(y_train)
 
-#val
-x_val_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(x_val)
-y_val_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(y_val)
+    #val
+    x_val_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(x_val)
+    y_val_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(y_val)
 
-#test
-x_test_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(x_test)
-y_test_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(y_test)
+    #test
+    x_test_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(x_test)
+    y_test_to_tensor_dataset = tf.data.Dataset.from_tensor_slices(y_test)
 
-## create the final tensor dataset
+    ## create the final tensor dataset
 
-train_ds = tf.data.Dataset.zip((x_train_to_tensor_dataset, y_train_to_tensor_dataset))
-val_ds = tf.data.Dataset.zip((x_val_to_tensor_dataset, y_val_to_tensor_dataset))
-test_ds = tf.data.Dataset.zip((x_test_to_tensor_dataset, y_test_to_tensor_dataset))
+    train_ds = tf.data.Dataset.zip((x_train_to_tensor_dataset, y_train_to_tensor_dataset))
+    val_ds = tf.data.Dataset.zip((x_val_to_tensor_dataset, y_val_to_tensor_dataset))
+    test_ds = tf.data.Dataset.zip((x_test_to_tensor_dataset, y_test_to_tensor_dataset))
 
 
-## create batches
+    ## create batches
 
-train_ds_batch = train_ds.batch(32)
-val_ds_batch = val_ds.batch(32)
-test_ds_batch = test_ds.batch(32)
+    train_ds_batch = train_ds.batch(bs)
+    val_ds_batch = val_ds.batch(bs)
+    test_ds_batch = test_ds.batch(bs)
+
+    return train_ds_batch,val_ds_batch,test_ds_batch
 
 
 
