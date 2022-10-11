3fe1938fd835e73ed989cefe9e822cc1b9ea1117
hejtlo0p <peter.stegeby@gmail.com>
2022-10-11 17:21:03 +0200

    removed unnecessary functionality and class members

--- backend/API/models/employee.py
+++ backend/API/models/employee.py
@@ -1,6 +1,4 @@
 from datetime import date
-import datetime
-import time
 from models.player import Player
 
 today       = date.today()
@@ -13,35 +11,35 @@ today_day   = int(now[-2:])
 
 class Employee():
 
-    def __init__(self, id, firstname, lastname, birthdate, connection):
-        self.id                 = id
-        self.firstname              = firstname
-        self.lastname              = lastname
+    def __init__(self, emp_id, firstname, lastname, birthdate):
+        self.emp_id             = emp_id
+        self.firstname          = firstname
+        self.lastname           = lastname
         self.SSN                = birthdate
-        self.player             = self.createPlayer(connection)
+        self.player             = self.createPlayer()
         self.birthYear          = int(birthdate[:4])
         self.birthMonth         = int(birthdate[4:6])
         self.birthDay           = int(birthdate[-2:])
         self.haveHadBirthday    = False if today_month < self.birthMonth else False if today_month == self.birthMonth and today_day < self.birthDay else True 
         self.age                = today_year - self.birthYear if self.haveHadBirthday else today_year - (self.birthYear+1)
-        self.birthDate = date(year=int(birthdate[:4]), month=int(birthdate[4:6]), day=int(birthdate[6:8]))
-        self.birthDateText = self.birthDate.strftime("%d %b, %Y")
-        self.birthDate = self.birthDate.strftime("%Y-%m-%d")
-        self.daysUntilBirthday = (date(year=today_year+1, month=self.birthMonth, day=self.birthDay) - date.today()).days if self.haveHadBirthday else (date(year=today_year, month=self.birthMonth, day=self.birthDay) - date.today()).days
+        self.birthDate          = date(year=int(birthdate[:4]), month=int(birthdate[4:6]), day=int(birthdate[6:8]))
+        self.birthDateText      = self.birthDate.strftime("%d %b, %Y")
+        self.birthDate          = self.birthDate.strftime("%Y-%m-%d")
+        self.daysUntilBirthday  = (date(year=today_year+1, month=self.birthMonth, day=self.birthDay) - date.today()).days if self.haveHadBirthday else (date(year=today_year, month=self.birthMonth, day=self.birthDay) - date.today()).days
 
     def getSQLData(self):
-        return (self.id, self.firstname, self.lastname, self.SSN)
+        return (self.firstname, self.lastname, self.SSN)
 
-    def createPlayer(self,connection):
-        playerID        = Player.getLatestPlayerId(connection) + 1
+    def createPlayer(self):
+        emp_id          = self.emp_id
         displayName     = f"{self.firstname.capitalize()} {self.lastname.capitalize()[0]}"
         ranking         = 1
         level           = 1
         xp_total        = 0
         xp_month        = 0
         last_login      = date.today().strftime("%Y-%m-%d")
-        consecutive_days=0
+        consecutive_days= 0
         birthday_today  = False
 
-        return Player(playerID, self.id, ranking, level,xp_total, xp_month,  last_login, consecutive_days, displayName, birthday_today)
+        return Player(emp_id, ranking, level, xp_total, xp_month,  last_login, consecutive_days, displayName, birthday_today)
     
--- backend/API/models/player.py
+++ backend/API/models/player.py
@@ -1,12 +1,6 @@
 import json
 import datetime
 
-def formatTupleIntoStr(tuple):
-        result = str(tuple)
-        for character in ')(,':
-                result = result.replace(character, '')
-        return result
-
 def json_default(value):
         if isinstance(value, datetime.date):
             return dict(year=value.year, month=value.month, day=value.day)
@@ -14,9 +8,8 @@ def json_default(value):
             return value.__dict__
 
 class Player():
-    def __init__(self, playerId, ranking, level, empId, xpTotal, xpMonth, last_login, consecutive_days, displayName, birthday_today):
-        self.playerId       = playerId
-        self.empId          = empId
+    def __init__(self, emp_id, ranking, level, xpTotal, xpMonth, last_login, consecutive_days, displayName, birthday_today):
+        self.emp_id         = emp_id
         self.ranking        = ranking
         self.level          = level
         self.xpTotal        = xpTotal
@@ -27,18 +20,8 @@ class Player():
         self.birthday_today = birthday_today
     
     def getSQLData(self):
-        return (self.playerId ,self.empId, self.ranking, self.level,self.xpTotal, self.xpMonth, self.last_login, self.consecutive_days) 
+        return (self.emp_id, self.ranking, self.level, self.xpTotal, self.xpMonth, self.last_login, self.consecutive_days) 
 
-    def getLatestPlayerId(connection):
-        with connection.cursor() as cursor:
-            sql = "SELECT player_ID FROM Player ORDER BY player_ID DESC LIMIT 1";
-            cursor.execute(sql)
-            if cursor.fetchone() is None:
-                return 0
-            result = formatTupleIntoStr(cursor.fetchone())
-            return int(result)
-
-    
     def toJSON(self):
         return json.dumps(self, default=json_default, 
             sort_keys=True, indent=4)        
