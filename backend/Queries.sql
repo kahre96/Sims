232598a8367d5cae9a80a34ec547709346ff81ad
hejtlo0p <peter.stegeby@gmail.com>
2022-10-12 13:17:25 +0200

    Added function to take pictures from admin site

--- backend/Queries.sql
+++ backend/Queries.sql
@@ -11,3 +11,6 @@ WHERE e.emp_ID = p.emp_ID AND p.ranking_id = r.ranking_id;
 SELECT p.emp_id, CONCAT(e.firstname," ",e.lastname) "Display name", r.name "Rank", p.xp_total, xp_month FROM employee e, player p, ranking r
 WHERE e.emp_id=1 AND (p.emp_id = e.emp_id) AND (p.ranking_id = r.ranking_id);
 
+
+SELECT emp_id, firstname, lastname FROM employee ORDER BY emp_id DESC LIMIT 5;
+
