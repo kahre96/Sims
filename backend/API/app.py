4deb524cbf652b19b7e1b6046683a33942fb6119
GlennVerhaag <gaver1998@gmail.com>
2022-10-13 10:53:21 +0200

    Merge

--- backend/API/app.py
+++ backend/API/app.py
@@ -4,10 +4,10 @@ from controllers.employeeController import employeecontroller
 from controllers.adminController import admincontroller
 from controllers.playerController import playercontroller
 
-db_password    = 'admin1'
-db_name        = 'emp_info'
-#db_password    = ''
-#db_name        = 'sims'
+#db_password    = 'admin1'
+#db_name        = 'emp_info'
+db_password    = ''
+db_name        = 'sims'
 
 # Define App and open connection to the mysql database
 app = Flask(__name__)
